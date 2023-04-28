f80757647fd5bf3f0c1e2722715c976e21028c17
Trong Nghia Tran <81551215+ttn1905@users.noreply.github.com>
2023-04-28 14:16:18 -0500

    Add files via upload

---
+++ tests/setting.test.js
@@ -0,0 +1,59 @@
+const request = require('supertest');
+const setting = require('../setting'); // assuming this is the file containing the Express app
+
+describe('POST /setting', () => {
+  it('updates user information and redirects to the settings page', async () => {
+    // prepare a mock user object
+    const mockUser = {
+      name: 'John Deer',
+      email: 'john@gmail.com',
+      address: '123 Main st.',
+      city: 'Houston',
+      state: 'TX',
+      zipcode: 12345,
+      user_name: 'John123',
+      password: '123',
+      cardNumber: '123456789',
+      cardExp: '11/27',
+      cvv: 312
+    };
+
+    // create a mock user in the database
+    await request(setting)
+      .post('/create-user') // assuming this route creates a new user
+      .send(mockUser);
+
+    // send a mock POST request to update the user information
+    const response = await request(setting)
+      .post('/setting')
+      .send({
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+        user_name: 'johndoe',
+        address: '456 Oak St.',
+        city: 'Dallas',
+        state: 'TX',
+        zipcode: 54321,
+        cardNumber: '987654321',
+        expirationDate: '10/30',
+        cvv: 456
+      });
+
+    // expect the response to redirect to the settings page
+    expect(response.statusCode).toBe(302);
+    expect(response.headers.location).toBe('/setting');
+
+    // check that the user information was updated in the database
+    const updatedUser = await User.findOne({ id: mockUser.id });
+    expect(updatedUser.name).toBe('John Doe');
+    expect(updatedUser.email).toBe('john.doe@gmail.com');
+    expect(updatedUser.user_name).toBe('johndoe');
+    expect(updatedUser.address).toBe('456 Oak St.');
+    expect(updatedUser.city).toBe('Dallas');
+    expect(updatedUser.state).toBe('TX');
+    expect(updatedUser.zipcode).toBe(54321);
+    expect(updatedUser.cardNumber).toBe('987654321');
+    expect(updatedUser.cardExp).toBe('10/30');
+    expect(updatedUser.cvv).toBe(456);
+  });
+});
